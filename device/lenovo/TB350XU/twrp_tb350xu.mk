d92ad867c2a9f85e0ba445c2f778b6adf58ac934
ventopm <ventopm@protonmail.com>
2025-07-10 09:03:59 +0530

    Create twrp_tb350xu.mk

---
+++ device/lenovo/TB350XU/twrp_tb350xu.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/lenovo/TB350XU/device.mk)
+
+PRODUCT_DEVICE := TB350XU
+PRODUCT_NAME := twrp_TB350XU
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := Lenovo Tab P11 Gen 2
+PRODUCT_MANUFACTURER := lenovo
